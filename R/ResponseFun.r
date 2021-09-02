928ffb929e57d817c174f16c0cd441cc4f2da86d
Shenghai Dai <s.dai@wsu.edu>
2021-09-02 13:50:57 +0000

    version 2.2

--- R/ResponseFun.r
+++ R/ResponseFun.r
@@ -21,6 +21,7 @@
 ResponseFun<-function (test.data, Mvalue="NA",max.score=1,round.decimal=0) {
   
   if (Mvalue == "NA") {
+    data.orig<-test.data
     x.plus<-rowSums(test.data,na.rm = T)
     R.est<-rowMeans(test.data,na.rm=T)*(ncol(test.data)-1)
     R.left<-floor(R.est)
@@ -31,20 +32,27 @@ ResponseFun<-function (test.data, Mvalue="NA",max.score=1,round.decimal=0) {
     for (i in 1:nrow(test.data)) { 
       for (j in 1:ncol(test.data)) { 
         if (!is.na(test.data[i,j])) next
-        x.r<-rowSums(test.data[,-j],na.rm = T)
-        x.r.na<-x.plus-test.data[,j]
+        x.r<-rowSums(data.orig[,-j],na.rm = T)
+        x.r.na<-x.plus-data.orig[,j]
         for (k in 1:max.score) {
-          p.mat.left[k,j]<-length(which(R.left==x.r & test.data[,j]==k))/length(which(test.data[,j]==k))
-          p.mat.right[k,j]<-length(which(R.right==x.r & test.data[,j]==k))/length(which(test.data[,j]==k))
+          p.mat.left[k,j]<-length(which(R.left==x.r.na & data.orig[,j]==k))/length(which(R.left==x.r.na))
+          p.mat.right[k,j]<-length(which(R.right==x.r.na & data.orig[,j]==k))/length(which(R.left==x.r.na))
           p.i.j[k]<-p.mat.left[k,j]+((p.mat.right[k,j]-p.mat.left[k,j])*(x.r[i]-R.left[i]))
         }
         p.i.j.tp1<-p.i.j.tp2<-rep(0,max.score+1)
         p.i.j.tp1[2:(max.score+1)]<-p.i.j.tp2[1:max.score]<-p.i.j
         p.i.j.tp<-p.i.j.tp1-p.i.j.tp2
         p.i.j.fn<-p.i.j.tp[-1]
-        if (is.na(test.data[i,j])) {test.data[i,j]<-rbinom(1,match(max(p.i.j.fn),p.i.j.fn),max(p.i.j.fn))}
+        size<-match(max(p.i.j.fn),p.i.j.fn)
+        if (max(p.i.j.fn)>1) {
+          prob<-1
+        } else if (max(p.i.j.fn)<0) {
+          prob<-0 
+        } else {prob<-max(p.i.j.fn)}
+        if (is.na(test.data[i,j])) {test.data[i,j]<-rbinom(1,size,prob)}
       }}
   } else {test.data[test.data==Mvalue]<-NA
+  data.orig<-test.data
   x.plus<-rowSums(test.data,na.rm = T)
   R.est<-rowMeans(test.data,na.rm=T)*(ncol(test.data)-1)
   R.left<-floor(R.est)
@@ -54,20 +62,26 @@ ResponseFun<-function (test.data, Mvalue="NA",max.score=1,round.decimal=0) {
   p.i.j<-rep(NA,max.score)
   for (i in 1:nrow(test.data)) { 
     for (j in 1:ncol(test.data)) { 
-      if (is.na(test.data[i,j])) next
-      x.r<-rowSums(test.data[,-j],na.rm = T)
-      x.r.na<-x.plus-test.data[,j]
+      if (!is.na(test.data[i,j])) next
+      x.r<-rowSums(data.orig[,-j],na.rm = T)
+      x.r.na<-x.plus-data.orig[,j]
       for (k in 1:max.score) {
-        p.mat.left[k,j]<-length(which(R.left==x.r & test.data[,j]==k))/length(which(test.data[,j]==k))
-        p.mat.right[k,j]<-length(which(R.right==x.r & test.data[,j]==k))/length(which(test.data[,j]==k))
+        p.mat.left[k,j]<-length(which(R.left==x.r.na & data.orig[,j]==k))/length(which(R.left==x.r.na))
+        p.mat.right[k,j]<-length(which(R.right==x.r.na & data.orig[,j]==k))/length(which(R.left==x.r.na))
         p.i.j[k]<-p.mat.left[k,j]+((p.mat.right[k,j]-p.mat.left[k,j])*(x.r[i]-R.left[i]))
       }
       p.i.j.tp1<-p.i.j.tp2<-rep(0,max.score+1)
       p.i.j.tp1[2:(max.score+1)]<-p.i.j.tp2[1:max.score]<-p.i.j
       p.i.j.tp<-p.i.j.tp1-p.i.j.tp2
       p.i.j.fn<-p.i.j.tp[-1]
-      if (is.na(test.data[i,j])){test.data[i,j]<-rbinom(1,match(max(p.i.j.fn),p.i.j.fn),max(p.i.j.fn))}
-      }}}
+      size<-match(max(p.i.j.fn),p.i.j.fn)
+      if (max(p.i.j.fn)>1) {
+        prob<-1
+      } else if (max(p.i.j.fn)<0) {
+        prob<-0 
+      } else {prob<-max(p.i.j.fn)}
+      if (is.na(test.data[i,j])) {test.data[i,j]<-rbinom(1,size,prob)}
+    }}}
   test.data<-round(test.data,digits=round.decimal)
   test.data<-as.data.frame(test.data)
     return(test.data)
